18124380fd6a0b6c82b53e890d52516b2dad5630
gurieveugen <seejay@MacBook-Pro-SeeJay.local>
2015-01-22 15:39:54 +0200

    1.15
    Set bootstrap

--- wp-content/themes/wheels/app/admin/initialize.php
+++ wp-content/themes/wheels/app/admin/initialize.php
@@ -5,6 +5,8 @@
 // ==============================================================
 Asset::add('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js');
 Asset::add('theme-style', TDU.'/css/style.css');
+Asset::add('bootstrap', TDU.'/bootstrap-3.3.2/less/bootstrap.css');
+Asset::add('fonts', TDU.'/fonts/stylesheet.css');
 
 // ==============================================================
 // Theme support
